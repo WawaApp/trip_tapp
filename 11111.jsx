d7f5e264e0c0c3e71b36af32a91d33ac274d2868
wawatapp <greatwany77@gmail.com>
2022-11-29 15:56:59 +0900

    test2

---
+++ 11111.jsx
@@ -0,0 +1,15 @@
+var txt1 = 'text1';
+console.log(txt1);
+
+if (true) {
+  var txt2 = 'text2';
+  console.log(txt2);
+}
+
+console.log(txt1);
+console.log(txt2);
+
+let txt1_1 = 'text1_1';
+const txt3 = 'hello';
+
+let txt1_1 = 'text1_1';
